eddaa97bff251af0067d4a492f445c0a7b5c42ed
thisissamiam <sam.kibiger@hak12.org>
2026-05-19 13:32:39 -0400

    push

--- justarandomprogram.py
+++ justarandomprogram.py
@@ -357,7 +357,7 @@ while True:
             pass
         choices = data[level]['choices']
         for i in range(len(choices)):
-            print(f"{str(i+1)}) {choices['text']}")
+            print(f"{str(i+1)}) {choices[i]['text']}")
 
 
     # elif answer.lower() == 's':

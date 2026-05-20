85b66e23a3ada6cb8c6745e178a4b09c83f591b6
thisissamiam <sam.kibiger@hak12.org>
2026-05-20 14:01:43 -0400

    fix bug

--- justarandomprogram.py
+++ justarandomprogram.py
@@ -121,7 +121,7 @@ while True:
     choices = data[level]['choices']
     for i in range(len(choices)):
         print(f"{str(i+1)}) {choices[i]['text']}")
-    print(str(len(choices))+ ') Stats')
+    print(str((len(choices))+1) + ') Stats')
     answer = input()
     try:
         answer = int(answer)

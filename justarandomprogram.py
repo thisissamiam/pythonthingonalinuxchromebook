b2c7e8fd3e38ce0e35937976fba01ca07c5c9fca
thisissamiam <sam.kibiger@hak12.org>
2026-05-20 11:43:23 -0400

    adding a dev skip so i dont have to go through the start every time

--- justarandomprogram.py
+++ justarandomprogram.py
@@ -40,45 +40,49 @@ print(r'''
 █                                                                            █
 █                                                                            █
 ██████████████████████████████████████████████████████████████████████████████''')
+skip = False
 answer = input()
 if answer.lower() == 'good':
   write('Perfect')
   time.sleep(1)
   os.system('clear')
+elif answer.lower() == 'skip': # easy dev way to skip instructions
+    skip = True
 else:
   write("You didn't follow the instructions. They may be cut off your screen. Try resizing your output/terminal")
   exit()
-write('Do you want the instructions? (y,n)')
-answer = input()
-if answer == 'y':
-    os.system('clear')
-    write('-----------------')
-    write('Instructions:')
-    write('You will be given a question.')
-    write('It will have multiple answer choices.')
-    write('You must say the number you want')
-    write('For example, 1, then hit enter')
-    write('To save the game, type S instead of a choice')
-    write('Good luck!')
-    write('-----------------')
-# Check if the answer is not no
-elif answer != 'n':
-    write('You picked neither y or n!!')
-    # stop the program if the answer is not no
-    exit(0)
-# will only run if answer is no or when the instructions finish
-write('Do you have a save file to load from? (y,n)')
-level = 'WakeUp' # need to put it anywhere before save and must run so it doesn't get overwriten
-answer = input()
-if answer == 'y':
-  write('Please paste your save code.')
-  answer = input()
-  layer = answer
-elif answer == 'n':
-  write('Ok, starting a new game')
-  os.system('clear')
-else:
-  exit()
+if not skip:
+    write('Do you want the instructions? (y,n)')
+    answer = input()
+    if answer == 'y':
+        os.system('clear')
+        write('-----------------')
+        write('Instructions:')
+        write('You will be given a question.')
+        write('It will have multiple answer choices.')
+        write('You must say the number you want')
+        write('For example, 1, then hit enter')
+        write('To save the game, type S instead of a choice')
+        write('Good luck!')
+        write('-----------------')
+    # Check if the answer is not no
+    elif answer != 'n':
+        write('You picked neither y or n!!')
+        # stop the program if the answer is not no
+        exit(0)
+    # will only run if answer is no or when the instructions finish
+    write('Do you have a save file to load from? (y,n)')
+    level = 'WakeUp' # need to put it anywhere before save and must run so it doesn't get overwriten
+    answer = input()
+    if answer == 'y':
+        write('Please paste your save code.')
+        answer = input()
+        layer = answer
+    elif answer == 'n':
+        write('Ok, starting a new game')
+        os.system('clear')
+    else:
+        exit()
 
 # art = {
 #   'inbedlayingup': r'''

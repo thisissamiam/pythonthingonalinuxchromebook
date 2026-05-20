d76fa6c69b22d3707891801a20791d926be969a6
thisissamiam <sam.kibiger@hak12.org>
2026-05-20 13:23:17 -0400

    awd

--- justarandomprogram.py
+++ justarandomprogram.py
@@ -3,88 +3,88 @@ import random
 import os
 import yaml
 
-def write(input):
-    print()
-    total = ''
-    for i in input:
-        total += i
-        print(total)
-        num = (random.randint(0,1))*.10
-        time.sleep(num)
-        print('\033[A\033[A')
-    print('')
-layer = '1'
-os.system('clear')
-write('Hello!')
-write('Welcome')
-write('to...')
-write('justarandomprogram!!!')
-time.sleep(1)
-os.system('clear')
-write('A image will appear on your screen. Please make sure it fits. Please say, "good" to confirm.')
-size = ''
-print(r'''
-██████████████████████████████████████████████████████████████████████████████
-█                                                                            █
-█                                                                            █
-█                                                                            █
-█                                                                            █
-█                                                                            █
-█                                                                            █
-█                                                                            █
-█                                                                            █
-█                                                                            █
-█                                                                            █
-█                                                                            █
-█                                                                            █
-█                                                                            █
-█                                                                            █
-██████████████████████████████████████████████████████████████████████████████''')
-skip = False
-loadedfromsave = False # avoid not defined error
-level = 'WakeUp' # need to put it anywhere before save and must run so it doesn't get overwriten
-answer = input()
-if answer.lower() == 'good':
-  write('Perfect')
-  time.sleep(1)
-  os.system('clear')
-elif answer.lower() == 'skip': # easy dev way to skip instructions
-    skip = True
-else:
-  write("You didn't follow the instructions. They may be cut off your screen. Try resizing your output/terminal")
-  exit()
-if not skip:
-    write('Do you want the instructions? (y,n)')
-    answer = input()
-    if answer == 'y':
-        os.system('clear')
-        write('-----------------')
-        write('Instructions:')
-        write('You will be given a question.')
-        write('It will have multiple answer choices.')
-        write('You must say the number you want')
-        write('For example, 1, then hit enter')
-        write('To save the game, type S instead of a choice')
-        write('Good luck!')
-        write('-----------------')
-    # Check if the answer is not no
-    elif answer != 'n':
-        write('You picked neither y or n!!')
-        # stop the program if the answer is not no
-        exit(0)
-    # will only run if answer is no or when the instructions finish
-    write('Do you have a save file to load from? (y,n)')
-    answer = input()
-    if answer == 'y':
-        write('Please paste your save code.')
-        answer = input()
-        level = answer
-        loadedfromsave = True
-    elif answer == 'n':
-        write('Ok, starting a new game')
-        os.system('clear')
-    else:
-        exit()
+# def write(input):
+#     print()
+#     total = ''
+#     for i in input:
+#         total += i
+#         print(total)
+#         num = (random.randint(0,1))*.10
+#         time.sleep(num)
+#         print('\033[A\033[A')
+#     print('')
+# layer = '1'
+# os.system('clear')
+# write('Hello!')
+# write('Welcome')
+# write('to...')
+# write('justarandomprogram!!!')
+# time.sleep(1)
+# os.system('clear')
+# write('A image will appear on your screen. Please make sure it fits. Please say, "good" to confirm.')
+# size = ''
+# print(r'''
+# ██████████████████████████████████████████████████████████████████████████████
+# █                                                                            █
+# █                                                                            █
+# █                                                                            █
+# █                                                                            █
+# █                                                                            █
+# █                                                                            █
+# █                                                                            █
+# █                                                                            █
+# █                                                                            █
+# █                                                                            █
+# █                                                                            █
+# █                                                                            █
+# █                                                                            █
+# █                                                                            █
+# ██████████████████████████████████████████████████████████████████████████████''')
+# skip = False
+# loadedfromsave = False # avoid not defined error
+# level = 'WakeUp' # need to put it anywhere before save and must run so it doesn't get overwriten
+# answer = input()
+# if answer.lower() == 'good':
+#   write('Perfect')
+#   time.sleep(1)
+#   os.system('clear')
+# elif answer.lower() == 'skip': # easy dev way to skip instructions
+#     skip = True
+# else:
+#   write("You didn't follow the instructions. They may be cut off your screen. Try resizing your output/terminal")
+#   exit()
+# if not skip:
+#     write('Do you want the instructions? (y,n)')
+#     answer = input()
+#     if answer == 'y':
+#         os.system('clear')
+#         write('-----------------')
+#         write('Instructions:')
+#         write('You will be given a question.')
+#         write('It will have multiple answer choices.')
+#         write('You must say the number you want')
+#         write('For example, 1, then hit enter')
+#         write('To save the game, type S instead of a choice')
+#         write('Good luck!')
+#         write('-----------------')
+#     # Check if the answer is not no
+#     elif answer != 'n':
+#         write('You picked neither y or n!!')
+#         # stop the program if the answer is not no
+#         exit(0)
+#     # will only run if answer is no or when the instructions finish
+#     write('Do you have a save file to load from? (y,n)')
+#     answer = input()
+#     if answer == 'y':
+#         write('Please paste your save code.')
+#         answer = input()
+#         level = answer
+#         loadedfromsave = True
+#     elif answer == 'n':
+#         write('Ok, starting a new game')
+#         os.system('clear')
+#     else:
+#         exit()
 
 art = {
   'inbedlayingup': r'''

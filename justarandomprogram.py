0a7ce112371caad704517b488fc6faa876261994
thisissamiam <sam.kibiger@hak12.org>
2026-05-27 13:31:30 -0400

    fix bug

--- justarandomprogram.py
+++ justarandomprogram.py
@@ -144,7 +144,7 @@ while True:
             time.sleep(2)
     
     os.system('clear')
-    if stats in node:
+    if "stats" in node:
         for stat in stats:
             print(stat)
     write(node['text']) # write next question

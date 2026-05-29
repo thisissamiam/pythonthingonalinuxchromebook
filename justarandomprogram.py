8812f6e13292dadf3fa79ee8dcd3ee28c5fdaea0
thisissamiam <sam.kibiger@hak12.org>
2026-05-29 13:22:20 -0400

    fix

--- justarandomprogram.py
+++ justarandomprogram.py
@@ -30,183 +30,177 @@ def main(stdscr):
     curses.noecho()
     curses.cbreak()
     stdscr.keypad(True)
-    stdscr.nodelay(True)
-
-    stdscr.addstr('HELLO')
     stdscr.refresh()
-
-
-
-curses.wrapper(main)
-exit()
-write('What language?')
-for i,lan in enumerate(os.listdir('./locale/')):
-    write(f"{i+1}) {lan[:-5]}")
-answer = userinput()
-while not answer.isnumeric():
-    write('That is not a valid response..')
+    stdscr.addstr(5, 10, "What language?")
+    stdscr.refresh()
+    for i,lan in enumerate(os.listdir('./locale/')):
+        write(f"{i+1}) {lan[:-5]}")
     answer = userinput()
-answer = int(answer)-1
-try:
-    lan = os.listdir('./locale/')[answer]
-except:
-    write('YOU DID SOMETHIGN WRONG or the code did no idea')
-    exit()
-if os.path.isfile(f'./locale/{lan}'):
-    with open(f'./locale/{lan}', 'r') as file:
-        lan = yaml.safe_load(file)
-else:
-    print(f'./locale/{lan}')
-    exit()
-
-
-layer = '1'
-os.system('clear')
-for i in lan['GameIntruduction']:
-    write(i)
-time.sleep(1)
-os.system('clear')
-write('A image will appear on your screen. Please make sure it fits. Please say, "good" to confirm.')
-size = ''
-print(r'''
-██████████████████████████████████████████████████████████████████████████████
-█                                                                            █
-█                                                                            █
-█                                                                            █
-█                                                                            █
-█                                                                            █
-█                                                                            █
-█                                                                            █
-█                                                                            █
-█                                                                            █
-█                                                                            █
-█                                                                            █
-█                                                                            █
-█                                                                            █
-█                                                                            █
-██████████████████████████████████████████████████████████████████████████████''')
-skip = False
-loadedfromsave = False # avoid not defined error
-level = 'WakeUp' # need to put it anywhere before save and must run so it doesn't get overwriten
-answer = userinput()
-if answer.lower() == 'good':
-  write('Perfect')
-  time.sleep(1)
-  os.system('clear')
-elif answer.lower() == 'skip': # easy dev way to skip instructions
-    skip = True
-else:
-  write("You didn't follow the instructions. They may be cut off your screen. Try resizing your output/terminal")
-  exit()
-if not skip:
-    write('Do you want the instructions? (y,n)')
-    answer = userinput()
-    if answer == 'y':
-        os.system('clear')
-        write('-----------------')
-        write('Instructions:')
-        write('You will be given a question.')
-        write('It will have multiple answer choices.')
-        write('You must say the number you want')
-        write('For example, 1, then hit enter')
-        write('To save the game, type S instead of a choice')
-        write('Good luck!')
-        write('-----------------')
-    # Check if the answer is not no
-    elif answer != 'n':
-        write('You picked neither y or n!!')
-        # stop the program if the answer is not no
-        exit(0)
-    # will only run if answer is no or when the instructions finish
-    write('Do you have a save file to load from? (y,n)')
-    answer = userinput()
-    if answer == 'y':
-        write('Please paste your save code.')
+    while not answer.isnumeric():
+        write('That is not a valid response..')
         answer = userinput()
-        level = answer
-        loadedfromsave = True
-    elif answer == 'n':
-        write('Ok, starting a new game')
-        time.sleep(1)
-        os.system('clear')
+    answer = int(answer)-1
+    try:
+        lan = os.listdir('./locale/')[answer]
+    except:
+        write('YOU DID SOMETHIGN WRONG or the code did no idea')
+        exit()
+    if os.path.isfile(f'./locale/{lan}'):
+        with open(f'./locale/{lan}', 'r') as file:
+            lan = yaml.safe_load(file)
     else:
+        print(f'./locale/{lan}')
         exit()
 
-# FILE LOADING
 
-with open('./Data/start.yaml', 'r') as file:
-        data = yaml.safe_load(file)
-
-
-# GAME LOOP
-while True:
-    if level in data:
-        node = data[level]
-    else:
-        write(f"Error: {level} does not exist in yaml")
-        if loadedfromsave:
-            write('Bad save file.')
-        else:
-            write('This probally means the game ended, but this should not happen in the release.')
-        exit()
-    if 'intro' in node:
-        write(node['intro'])
-        intro = True
-    else:
-        intro = False
-    if 'introimage' in node:
-        path = './Assets/'+ data[level]['introimage']+ '.txt'
-        if os.path.isfile(path):
-            with open(path, 'r') as file:
-                print(file.read())
-            time.sleep(2)
-        else:
-            write(f"{path} does not exist!!!")
-    else:
-        if intro:
-            time.sleep(2)
-    
+    layer = '1'
     os.system('clear')
-    if "stats" in node:
-        for stat in node['stats']:
-            for key, value in stat.items():
-                stats[key] += value
-                write(f"{key} has changed by {value}")
-    write(node['text']) # write next question
-    if 'textimage' in node:
-        path = './Assets/' + node['textimage'] + '.txt'
-        if os.path.isfile(path):
-            with open(path, 'r') as file:
-                print(file.read())
-                print()
-    choices = data[level]['choices']
-    for i in range(len(choices)):
-        write(f"{str(i+1)}) {choices[i]['text']}")
-    write(str((len(choices))+1) + ') Stats')
+    for i in lan['GameIntruduction']:
+        write(i)
+    time.sleep(1)
+    os.system('clear')
+    write('A image will appear on your screen. Please make sure it fits. Please say, "good" to confirm.')
+    size = ''
+    print(r'''
+    ██████████████████████████████████████████████████████████████████████████████
+    █                                                                            █
+    █                                                                            █
+    █                                                                            █
+    █                                                                            █
+    █                                                                            █
+    █                                                                            █
+    █                                                                            █
+    █                                                                            █
+    █                                                                            █
+    █                                                                            █
+    █                                                                            █
+    █                                                                            █
+    █                                                                            █
+    █                                                                            █
+    ██████████████████████████████████████████████████████████████████████████████''')
+    skip = False
+    loadedfromsave = False # avoid not defined error
+    level = 'WakeUp' # need to put it anywhere before save and must run so it doesn't get overwriten
     answer = userinput()
-    if not answer.isnumeric():
-        if answer.lower() == 's':
-            write('Saving game...')
-            write(level)
-            write('The previous message is your save code')
-            exit()
-        else:
-            write("That's not a number!")
-            continue
-    answer = int(answer)
-    if answer == len(choices) + 1:
-        os.system('clear')
-        write('Your stats are...')
-        write(f'Health: {stats['PlayerHealth']}')
-        write(f'Attack: {stats['PlayerAttack']}')
-        write(f'PlayerSoulPoint: {stats['PlayerSoulPoint']}')
-        write(f'Intelligence: {stats['PlayerIntelligence']}')
-        write(f'PlayerSpeed: {stats['PlayerSpeed']}')
-        write(f'Money: {stats['PlayerMoney']}')
-        print()
-        write('Press enter to continue.')
-        userinput()
-    else:
-        answer -= 1
-        level = (choices[answer]['result'])
+    if answer.lower() == 'good':
+    write('Perfect')
+    time.sleep(1)
     os.system('clear')
+    elif answer.lower() == 'skip': # easy dev way to skip instructions
+        skip = True
+    else:
+    write("You didn't follow the instructions. They may be cut off your screen. Try resizing your output/terminal")
+    exit()
+    if not skip:
+        write('Do you want the instructions? (y,n)')
+        answer = userinput()
+        if answer == 'y':
+            os.system('clear')
+            write('-----------------')
+            write('Instructions:')
+            write('You will be given a question.')
+            write('It will have multiple answer choices.')
+            write('You must say the number you want')
+            write('For example, 1, then hit enter')
+            write('To save the game, type S instead of a choice')
+            write('Good luck!')
+            write('-----------------')
+        # Check if the answer is not no
+        elif answer != 'n':
+            write('You picked neither y or n!!')
+            # stop the program if the answer is not no
+            exit(0)
+        # will only run if answer is no or when the instructions finish
+        write('Do you have a save file to load from? (y,n)')
+        answer = userinput()
+        if answer == 'y':
+            write('Please paste your save code.')
+            answer = userinput()
+            level = answer
+            loadedfromsave = True
+        elif answer == 'n':
+            write('Ok, starting a new game')
+            time.sleep(1)
+            os.system('clear')
+        else:
+            exit()
+
+    # FILE LOADING
+
+    with open('./Data/start.yaml', 'r') as file:
+            data = yaml.safe_load(file)
+
+
+    # GAME LOOP
+    while True:
+        if level in data:
+            node = data[level]
+        else:
+            write(f"Error: {level} does not exist in yaml")
+            if loadedfromsave:
+                write('Bad save file.')
+            else:
+                write('This probally means the game ended, but this should not happen in the release.')
+            exit()
+        if 'intro' in node:
+            write(node['intro'])
+            intro = True
+        else:
+            intro = False
+        if 'introimage' in node:
+            path = './Assets/'+ data[level]['introimage']+ '.txt'
+            if os.path.isfile(path):
+                with open(path, 'r') as file:
+                    print(file.read())
+                time.sleep(2)
+            else:
+                write(f"{path} does not exist!!!")
+        else:
+            if intro:
+                time.sleep(2)
+        
+        os.system('clear')
+        if "stats" in node:
+            for stat in node['stats']:
+                for key, value in stat.items():
+                    stats[key] += value
+                    write(f"{key} has changed by {value}")
+        write(node['text']) # write next question
+        if 'textimage' in node:
+            path = './Assets/' + node['textimage'] + '.txt'
+            if os.path.isfile(path):
+                with open(path, 'r') as file:
+                    print(file.read())
+                    print()
+        choices = data[level]['choices']
+        for i in range(len(choices)):
+            write(f"{str(i+1)}) {choices[i]['text']}")
+        write(str((len(choices))+1) + ') Stats')
+        answer = userinput()
+        if not answer.isnumeric():
+            if answer.lower() == 's':
+                write('Saving game...')
+                write(level)
+                write('The previous message is your save code')
+                exit()
+            else:
+                write("That's not a number!")
+                continue
+        answer = int(answer)
+        if answer == len(choices) + 1:
+            os.system('clear')
+            write('Your stats are...')
+            write(f'Health: {stats['PlayerHealth']}')
+            write(f'Attack: {stats['PlayerAttack']}')
+            write(f'PlayerSoulPoint: {stats['PlayerSoulPoint']}')
+            write(f'Intelligence: {stats['PlayerIntelligence']}')
+            write(f'PlayerSpeed: {stats['PlayerSpeed']}')
+            write(f'Money: {stats['PlayerMoney']}')
+            print()
+            write('Press enter to continue.')
+            userinput()
+        else:
+            answer -= 1
+            level = (choices[answer]['result'])
+        os.system('clear')
+curses.wrapper(main)
